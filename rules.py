b5c800a40ba3f205e2c2f9e51d0192c486a1e6cc
Raunaq Singh Gandhi <raunaq@Raunaqs-MacBook-Air-2.local>
2022-09-25 21:53:02 +0100

    rules

---
+++ rules.py
@@ -0,0 +1,12 @@
+print("These are the rules")
+
+#if its a new repo git clone copy paste
+#ls -la to see everything 
+# save your progress , command s
+# git status : to check the status
+# git add .  : to save/add your progress to git
+# git status again
+# git commit -m "d"
+# git push : to push it to github
+
+#print and it is saved
